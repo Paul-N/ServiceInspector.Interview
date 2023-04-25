bc1e4dcfae9416bc2aa99b35cdda759ea3072c44
EvilGenius <evilgenius@211.ru>
2023-04-25 18:17:02 +0300

    Maui custom control

---
+++ ServiceInspector.Interview02.Maui/Controls/BackgroundAwareButton.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Platform;
+
+namespace ServiceInspector.Interview02.Maui.Controls
+{
+    public class BackgroundAwareButton : Button
+    {
+        public static readonly BindableProperty CustomBackgroundColorProperty =
+            BindableProperty.Create(nameof(CustomBackgroundColor), typeof(Color), typeof(BackgroundAwareButton), propertyChanged: OnCustomBackgroundColorChanged);
+
+        public Color CustomBackgroundColor
+        {
+            get => (Color)GetValue(CustomBackgroundColorProperty);
+            set => SetValue(CustomBackgroundColorProperty, value);
+        }
+
+        static void OnCustomBackgroundColorChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (BackgroundAwareButton)bindable;
+            var backgroundColor = control.CustomBackgroundColor;
+
+            if (control.Handler is null || control.Handler.PlatformView is null)
+            {
+                // Workaround for when this executes the Handler and PlatformView is null
+                control.HandlerChanged += OnHandlerChanged;
+                return;
+            }
+
+            if (backgroundColor is not null)
+            {
+#if ANDROID
+                var nativeBtn = control.Handler.PlatformView as Android.Widget.Button;
+                if (nativeBtn != null)
+                {
+                    nativeBtn.SetBackgroundColor(backgroundColor.ToPlatform());
+                }
+#elif IOS || MACCATALYST
+                var nativeBtn = control.Handler.PlatformView as UIKit.UIButton;
+                if (nativeBtn != null)
+                {
+                    nativeBtn.BackgroundColor = backgroundColor.ToPlatform();
+                }
+#endif
+            }
+
+            void OnHandlerChanged(object s, EventArgs e)
+            {
+                OnCustomBackgroundColorChanged(control, oldValue, newValue);
+                control.HandlerChanged -= OnHandlerChanged;
+            }
+        }
+    }
+}
